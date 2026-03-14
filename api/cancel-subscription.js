2d8a52fa979eee156a878235089bb772bbbff3a3
hofsmog <hofsmog@gmail.com>
2026-03-14 15:06:41 +0100

    Update cancel-subscription.js

--- api/cancel-subscription.js
+++ api/cancel-subscription.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
   const { customerId, restaurantId } = req.body;
   const STRIPE_SECRET = process.env.STRIPE_SECRET_KEY;
